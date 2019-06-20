64de927c763cfc5dbcd5a12bcd3a56468d3bcbe1
Rea Yan <janenu22@gmail.com>
2019-06-20 22:42:52 +0800

    new function

--- read.py
+++ read.py
@@ -1,16 +1,18 @@
 data = []
 count = 0
-total_len = 0
+sum_len = 0
 line_len = 0
+new = []
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line.strip())
 		count += 1
 		line_len = len(line)
-		total_len = total_len + line_len
-		if count % 100000 == 0:
-			print(len(data))
+		sum_len += line_len
+		if len(line) < 100:
+			new.append(line)
 
 print(len(data))
-print('留言的平均長度是', int(total_len / len(data)))
+print('留言的平均長度是', int(sum_len / len(data)))
+print('小於100字符的留言有', len(new), '筆。')
 
